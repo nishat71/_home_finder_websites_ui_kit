959b14915f55cfb1cf0c02ad551722854373aa32
Nishat <Nishat@afms-MacBook-Pro.local>
2022-11-04 17:48:54 +0600

    done

---
+++ assets/js/custom.js
@@ -0,0 +1,20 @@
+let config = {
+    type:'loop',
+    arrows:false,
+    pagination:true,
+    perPage: 1,
+    perMove:1,
+    easing:'ease',
+};
+new Splide( '._home_finder_hero_slider',config ).mount();
+
+let config1 = {
+    type:'loop',
+    arrows:false,
+    pagination:true,
+    perPage: 1,
+    perMove:1,
+    easing:'ease',
+    
+};
+new Splide( '._home_finder_testimonial_slider',config1).mount();
